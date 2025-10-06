1c11bc194621e38bca01a386acffffe6d10438f9
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-06 13:04:24 +0530

    Updated dbConnect.js file

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Connected : ${mongoDbConnection.connection.host}`);
+    console.log(`Database Connected : ${mongoose.connection.host}`);
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
     process.exit(1);
